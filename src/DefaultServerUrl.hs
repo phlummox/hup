a305b735e1ed43f8fd5909feccf21814aa2aa575
phlummox <github16.phlummox@recursor.net>
2017-12-15 10:38:37 +0800

    add doc comment

--- src/DefaultServerUrl.hs
+++ src/DefaultServerUrl.hs
@@ -2,7 +2,7 @@
 
 module DefaultServerUrl where
 
-
+-- | The default hackage server URL.
 defaultServerUrl :: String
 defaultServerUrl = "https://hackage.haskell.org/"
 --defaultServerUrl = "http://localhost:8080/"
